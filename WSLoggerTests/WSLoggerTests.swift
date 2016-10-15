a416cc77afc880456dd649e256451776a17f963e
Ricardo Pereira <m@ricardopereira.eu>
2016-10-15 18:51:21 +0100

    More tests

--- WSLoggerTests/WSLoggerTests.swift
+++ WSLoggerTests/WSLoggerTests.swift
@@ -30,12 +30,53 @@ class WSLoggerTests: XCTestCase {
     override func setUp() {
         super.setUp()
         Recorder.clear()
+        WSLogger.shared.reset()
         WSLogger.shared.printable = MockPrintable()
     }
 
-    func testInternalLog() {
-        WSLogger.shared.log("Let's go!", level: .Debug, customAttributes: nil, className: String(self.dynamicType))
-        XCTAssertTrue(Recorder.messages[0] == "WSLoggerTests.swift:37 WSLoggerTests.testInternalLog() DEBUG \"Let's go!\" [nil]")
+    func testDefaultLog() {
+        WSLogger.shared.log("Let's go!")
+        if let message = Recorder.messages.first {
+            XCTAssertTrue(message == "DEBUG \"Let's go!\" [nil]")
+        }
+    }
+
+    func testDefaultLogIgnoresVerboseEntries() {
+        WSLogger.shared.log("Let's go!", level: .Verbose)
+        XCTAssertTrue(Recorder.messages.count == 0)
+    }
+
+    func testVerboseLog() {
+        let defaultLevel = WSLoggerOptions.defaultLevel
+        defer { WSLoggerOptions.defaultLevel = defaultLevel }
+        WSLoggerOptions.defaultLevel = .Verbose
+        WSLogger.shared.log("ZZZzzZzZZZ", level: .Verbose, customAttributes: nil, className: String(self.dynamicType))
+        if let message = Recorder.messages.first {
+            XCTAssertTrue(message == "VERBOSE \"ZZZzzZzZZZ\" [nil]")
+        }
+    }
+
+    func testCustomAttributesLog() {
+        WSLogger.shared.traceMethod = true
+        WSLogger.shared.log("Nice, really nice.", level: .Info, customAttributes: ["user": 4])
+        if let message = Recorder.messages.first {
+            XCTAssertTrue(message == "testCustomAttributesLog() INFO \"Nice, really nice.\" [Optional([\"user\": 4])]")
+        }
+    }
+
+    func testCompleteTraceLog() {
+        WSLogger.shared.traceFile = true
+        WSLogger.shared.traceMethod = true
+        WSLogger.shared.log("Nice, really nice.", level: .Info, customAttributes: nil, className: String(self.dynamicType))
+        if let message = Recorder.messages.first {
+            XCTAssertTrue(message == "WSLoggerTests.swift:70 WSLoggerTests.testCompleteTraceLog() INFO \"Nice, really nice.\" [nil]")
+        }
+    }
+
+    func testLoggerIgnoreClass() {
+        WSLogger.shared.ignoreClass(WSLoggerTests)
+        WSLogger.shared.log("Great!", level: .Verbose, customAttributes: nil, className: String(self.dynamicType))
+        XCTAssertTrue(Recorder.messages.count == 0)
     }
 
 }
