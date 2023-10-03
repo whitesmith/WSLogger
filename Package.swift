392b093a29a874becb0eafd8b65386dc92219ea1
Ricardo Pereira <ricardopereira@whitesmith.co>
2023-10-03 12:07:07 +0100

    Update Swift package.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "WSLogger",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
